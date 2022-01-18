8e1526860431250352225c8c3a88cd2639eeff3f
bz0 <bzz0217@yahoo.co.jp>
2022-01-18 10:45:26 +0900

    add:head追加

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function H() {
+    return (
+        <Head>
+            <title>DevLog</title>
+            <meta name="description" content="個人開発・勉強の進捗共有や分からないことを相談できる場所です" />
+            <meta name="google-site-verification" content="8HIVME_0baUdrcpSgZfgQP451n0xoHoqyntcwi0HU10" />
+            <meta property="og:title" content="DevLog" />
+            <meta property="og:description" content="個人開発・勉強の進捗共有や分からないことを相談できる場所です" />
+            <meta property="og:url" content="https://devlog.bz0.work" />
+            <meta property="og:image" content="" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="DevLog" />
+            <meta name="twitter:card" content="summary" /> 
+            <meta name="twitter:site" content="@bz0bz0bz0bz0" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+    )
+}
