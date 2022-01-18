10581dad2a1b98bf7d61ff297ce7f06a6e5a9de6
bz0 <bzz0217@yahoo.co.jp>
2022-01-18 10:44:30 +0900

    add:tailwindcss導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}', 
+    './src/features/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'xxs': '.5rem'
+      },
+      colors: {
+        'teal': colors.teal
+      }
+    },
   },
   plugins: [],
 }
