2cabcd24edb4da2136802a68c5f139c0c88a5192
bz0 <bzz0217@yahoo.co.jp>
2022-01-29 01:30:17 +0900

    update:グローバルナビの高さを調整

--- src/components/GlobalNav/index.tsx
+++ src/components/GlobalNav/index.tsx
@@ -43,7 +43,7 @@ export default function GlobalNav() {
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
+            <div className="relative flex items-center justify-between h-12">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -72,7 +72,7 @@ export default function GlobalNav() {
                         <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                           <span className="sr-only">Open user menu</span>
                           <img
-                            className="h-8 w-8 rounded-full"
+                            className="h-8 w-8 rounded-full border-gray-200 border-2"
                             src={user.avatar_url}
                             alt=""
                           />
