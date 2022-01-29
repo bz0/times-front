bb5fb8c630094b47e2f5b3c85a03fcc058d85b64
bz0 <bzz0217@yahoo.co.jp>
2022-01-29 14:31:17 +0900

    update:グローバルナビに遷移リスト追加

--- src/components/GlobalNav/index.tsx
+++ src/components/GlobalNav/index.tsx
@@ -12,6 +12,12 @@ const login = async () => {
   window.location.href = data.redirect_url;
 }
 
+const navigation = [
+  { name: 'ホーム', href: '/', current: true },
+  { name: '投稿', href: '/times', current: false },
+  { name: '質問一覧', href: '/qa', current: false }
+]
+
 function classNames(...classes:any) {
   return classes.filter(Boolean).join(' ')
 }
@@ -58,6 +64,24 @@ export default function GlobalNav() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center text-white font-bold">
                   DevLog
+                </div>
+
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
